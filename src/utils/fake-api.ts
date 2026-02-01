1ce4c3c3397fe38e26b7a9c61c9eb20f43aaf98f
Oksana Bessonov <48567090+oksankaa@users.noreply.github.com>
2026-02-01 18:39:17 +0100

    Update fake-api.ts

--- src/utils/fake-api.ts
+++ src/utils/fake-api.ts
@@ -1,17 +1,18 @@
 export function sendMessageToServer(
   text: string
-): Promise<{ id: string; text: string }> {
+): Promise<{ id: string text: string }> {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
-      const success = Math.random() < 0.5;
+      const success = Math.random() < 0.5
       if (!success) {
-        reject(new Error('Server exploded 💥'));
+        reject(new Error('Server exploded 💥'))
       } else {
         resolve({
           id: crypto.randomUUID(),
           text,
-        });
+        })
       }
-    }, 1000);
-  });
+    }, 1000)
+  })
 }
+
